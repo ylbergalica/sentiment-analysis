21b8bdf1b084e014a52c12f4ff07284ce194d29f
GalicaYlber <galica.ylber@gmail.com>
2023-08-23 12:34:06 +0200

    Add Analyzer Documentation
    -Added JSDoc Documentation comments

--- src/data/analyze.js
+++ src/data/analyze.js
@@ -2,6 +2,12 @@ import Sentiment from 'sentiment';
 
 const sentimentAnalyzer = new Sentiment();
 
+/**
+ * Performs sentiment analysis on the given text.
+ * @async
+ * @param {string} text - The text to be analyzed.
+ * @returns {Promise<object>} A promise that resolves to an object containing the resulting object.
+ */
 export default async (text) => {
 	return sentimentAnalyzer.analyze(text);
 };
